a6d1c342f2b64ea8d7b8a933dad56760ffc20a63
basvad <59355610+basvad@users.noreply.github.com>
2020-04-24 21:52:20 +0300

    Update handler.js

--- src/js/handler.js
+++ src/js/handler.js
@@ -1,43 +1,13 @@
-function setCookie(name, value, options = {}) {
-
-  options = {
-    path: '/',
-    // при необходимости добавьте другие значения по умолчанию
-    ...options
-  };
-
-  if (options.expires instanceof Date) {
-    options.expires = options.expires.toUTCString();
-  }
-
-  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-
-  for (let optionKey in options) {
-    updatedCookie += "; " + optionKey;
-    let optionValue = options[optionKey];
-    if (optionValue !== true) {
-      updatedCookie += "=" + optionValue;
-    }
-  }
-
-  document.cookie = updatedCookie;
-}
-
-// Пример использования:
-//setCookie('user', 'John', {secure: true, 'max-age': 3600});
-
 //получаем форму
 const form = document.querySelector('form');
 
 //вешаем обработчик события submit
 form.addEventListener('submit',function(event){
     // специальные символы (пробелы), требуется кодирование
-    //let name = "town";
+    let name = "town";
     //получаем значения инпута с приведением в нижний регистр
-    //let value = form.elements[0].value.toLowerCase();
-    //setCookie( name, value, {'max-age': 3600});
-    document.cookie = "name=oeschger";
-    alert(document.cookie); 
+    let value = form.elements[0].value.toLowerCase();
+    document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
     event.preventDefault();
     });
 
