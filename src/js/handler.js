84b5b21d95833a8a44383cbce0c3c4d4ea0c737c
basvad <59355610+basvad@users.noreply.github.com>
2020-04-25 12:11:35 +0300

    Update handler.js

--- src/js/handler.js
+++ src/js/handler.js
@@ -4,7 +4,31 @@ function deleteCookie(name) {
     'max-age': -1
   })
 }
+// функция установки куки
+function setCookie(name, value, options = {}) {
 
+  options = {
+    path: '/',
+    // при необходимости добавьте другие значения по умолчанию
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
 // возвращает куки с указанным name,
 // или undefined, если ничего не найдено
 function getCookie(name) {
