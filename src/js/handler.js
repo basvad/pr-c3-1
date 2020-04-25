54fc4bcf0962bab1a7ea7ec3256e8a5ec64a6406
basvad <59355610+basvad@users.noreply.github.com>
2020-04-25 13:01:20 +0300

    Update handler.js

--- src/js/handler.js
+++ src/js/handler.js
@@ -1,9 +1,14 @@
-//функция удаления куки
-function deleteCookie() {
-  //выставить время жизни cookie так, чтобы оно было просрочено и кука удалилась
-  document.cookie = "name=town; expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+//функция очистки кук
+function CookiesDelete() {
+	var cookies = document.cookie.split(";");
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i];
+		var eqPos = cookie.indexOf("=");
+		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+		document.cookie = name + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	}
 }
-
 // возвращает куки с указанным name,
 // или undefined, если ничего не найдено
 function getCookie(name) {
@@ -45,7 +50,7 @@ else{
     //вешаем обработчки на кнопку сброса куки
     document.getElementById('reset_cookies').addEventListener('click',function(){
       //удаляем куку 
-      document.cookie = "name=town; expires=Thu, 01 Jan 1970 00:00:00 GMT;";
+    CookiesDelete();
       //обновляемся
       location.reload();
     })
